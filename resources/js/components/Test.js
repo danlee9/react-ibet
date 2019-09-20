18fb7cb6713504e8fa815e73f98b754f82461691
Daniel Lee <dan.dh.lee@gmail.com>
2019-09-19 22:21:20 -0700

    Initial commit

---
+++ resources/js/components/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Test.css';
+
+const Test = () => {
+    return (
+        <div className="test">TESTTTTTTTTTTTTTTTTTTTT</div>
+    );
+}
+
+export default Test;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+        <title>iBet</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="js/app.js"></script>
+    </body>
+</html>

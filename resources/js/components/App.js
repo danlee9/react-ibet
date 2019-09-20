7f912621932b04473e62be335ba16ff091c371af
Daniel Lee <dan.dh.lee@gmail.com>
2019-09-20 00:34:00 -0700

    Change filenames from app.js to index.js. Also use root div instead of 'app' div

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Test from './Test';
+import Login from './Login';
 
 export default class App extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Making changes</div>
-
-                            <div className="card-body"><Test/></div>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <h1>TESTING</h1>
+                <Login />
             </div>
+            
         );
     }
 }

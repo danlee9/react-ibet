5c798296ec924942dab38c393e0db89adc8ee79b
Daniel Lee <dan.dh.lee@gmail.com>
2020-01-02 19:02:45 -0800

    Add script to remove dead games

--- app/Console/Commands/Script.php
+++ app/Console/Commands/Script.php
@@ -40,23 +40,34 @@ class Script extends Command
      */
     public function handle()
     {
-        // added data for new linking table between games and teams
-        $games = Game::all();
+        // // added data for new linking table between games and teams
+        // $games = Game::all();
+        // foreach ($games as $game) {
+        //     $game_id = $game->id;
+        //     if ($game_id == 1) {
+        //         continue;
+        //     } else {
+        //         $home_team = Team::where('full_name', $game->home_team)->first();
+        //         $away_team = Team::where('full_name', $game->away_team)->first();
+        //         $home_id = $home_team->id;
+        //         $away_id = $away_team->id;
+        //         // echo "Game Number {$game_id}: {$away_id} @ {$home_id}".PHP_EOL;
+        //         DB::table('game_team')->insert([
+        //             ['game_id' => $game_id, 'team_id' => $home_id],
+        //             ['game_id' => $game_id, 'team_id' => $away_id]
+        //         ]);
+        //     }
+        // }
+
+        // get all the games that are stuck to be removed
+        $from = date('2019-12-06');
+        $to = date('2019-12-31');
+        $games = Game::whereBetween('created_at', [$from, $to])->get();
         foreach ($games as $game) {
-            $game_id = $game->id;
-            if ($game_id == 1) {
-                continue;
-            } else {
-                $home_team = Team::where('full_name', $game->home_team)->first();
-                $away_team = Team::where('full_name', $game->away_team)->first();
-                $home_id = $home_team->id;
-                $away_id = $away_team->id;
-                // echo "Game Number {$game_id}: {$away_id} @ {$home_id}".PHP_EOL;
-                DB::table('game_team')->insert([
-                    ['game_id' => $game_id, 'team_id' => $home_id],
-                    ['game_id' => $game_id, 'team_id' => $away_id]
-                ]);
-            }
+            // print_r($game->game_status);
+            $game->game_status = 'completed';
+            $game->save();
         }
+        print_r('Completed');
     }
 }
